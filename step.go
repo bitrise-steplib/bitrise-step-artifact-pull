5960b52b42dc18778c096f657858c4113717f11c
Daniel Serleg <daniel.serleg@bitrise.io>
2021-11-30 17:26:11 +0100

    Wire the input parsing

--- step.go
+++ step.go
@@ -1,19 +1,26 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"strings"
+
 	"github.com/bitrise-io/go-steputils/stepconf"
 	"github.com/bitrise-io/go-utils/command"
 	"github.com/bitrise-io/go-utils/env"
 	"github.com/bitrise-io/go-utils/log"
+	"github.com/bitrise-steplib/bitrise-step-artifact-pull/model"
 )
 
 type Input struct {
-	Verbose bool `env:"verbose,required"`
+	Verbose         bool   `env:"verbose,required"`
+	ArtifactSources string `env:"artifact_source"`
 }
 
 type Config struct {
-	Verbose bool
+	Verbose         bool
+	ArtifactSources []string
+	FinishedStages  model.FinishedStages
 }
 
 type Result struct {
@@ -35,15 +42,31 @@ func (a ArtifactPull) ProcessConfig() (Config, error) {
 
 	stepconf.Print(input)
 
+	finishedStages := a.envRepository.Get("FINISHED_STAGES")
+
+	var finishedStagesModel model.FinishedStages
+	if err := json.Unmarshal([]byte(finishedStages), &finishedStagesModel); err != nil {
+		return Config{}, fmt.Errorf("failed to parse step inputs: %w", err)
+	}
+
 	// TODO: validate inputs here and possibly convert from string to a concrete type
 	//nolint:gosimple
 	return Config{
-		Verbose: input.Verbose,
+		Verbose:         input.Verbose,
+		ArtifactSources: strings.Split(input.ArtifactSources, ","),
+		FinishedStages:  finishedStagesModel,
 	}, nil
 }
 
 func (a ArtifactPull) Run(cfg Config) (Result, error) {
 	a.logger.EnableDebugLog(cfg.Verbose)
+	buildIdGetter := NewDefaultBuildIDGetter(cfg.FinishedStages, cfg.ArtifactSources)
+	buildIDs, err := buildIdGetter.GetBuildIDs()
+	if err != nil {
+		return Result{}, err
+	}
+
+	a.logger.Printf("%+v", buildIDs)
 	// TODO
 	return Result{}, nil
 }
