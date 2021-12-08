edd9e0528a7a13010a16142048457bb9d64a0d86
Imre Kelényi <imre.kelenyi@gmail.com>
2021-12-08 15:49:38 +0100

    new secret env var

--- e2e/get_access_token.sh
+++ e2e/get_access_token.sh
@@ -4,7 +4,7 @@ set -ex
 
 json_response=$(curl -X POST https://auth.services.bitrise.dev/auth/realms/bitrise-services/protocol/openid-connect/token -k \
     --data "client_id=artifact-pull" \
-    --data "client_secret=$ARTIFACT_PULL_AUTH_CLIENT_SECRET" \
+    --data "client_secret=$BITRISE_AUTH_SERVICE_ARTIFACT_PULL_CLIENT_SECRET" \
     --data "grant_type=urn:ietf:params:oauth:grant-type:uma-ticket" \
     --data "scope=build_artifact:read build:read app:read" \
     --data "claim_token=eyJidWlsZF9pZHMiOlsiNzNkMzNmYjUtMzVjNi00OTVmLWJkODAtMDE1YWU2ODFkYjMzIiwiMzk0MDRiZWUtNTJiYS00Y2EyLTg1MDgtOTE0ODllN2Y2YWZhIiwiZWQwZGEwY2YtNjZjYy00MTA5LWIyM2YtOGExNTZkNjFiMGMzIiwiMDUxMzBjZTQtODI1Yi00Y2ExLWE5YmUtNGY1NDQxM2U1ZGNkIl0sInBpcGVsaW5lX2lkIjpbIjM2ZTg1NDBkLTQxYzctNDNjZS05NzRiLTgyNTQ5OTc2OTkxZSJdfQ==" \
