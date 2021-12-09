32a14a98098147611a7913d0f47dcb8a8d40a5ac
Daniel Serleg <daniel.serleg@bitrise.io>
2021-12-09 12:11:59 +0100

    Use the provided client

--- downloader/file_downloader.go
+++ downloader/file_downloader.go
@@ -1,32 +1,40 @@
 package downloader
 
 import (
+	"context"
 	"fmt"
-	"io"
-	"net/http"
+	"time"
 
+	"github.com/bitrise-io/go-steputils/input"
+	"github.com/bitrise-io/go-utils/filedownloader"
 	"github.com/bitrise-io/go-utils/log"
+	"github.com/bitrise-io/go-utils/retry"
 )
 
 type FileDownloader interface {
-	DownloadFileFromURL(url string) (io.ReadCloser, error)
+	DownloadFileFromURL(url string) ([]byte, error)
 }
 
 type DefaultFileDownloader struct {
 	Logger log.Logger
 }
 
-func (fd *DefaultFileDownloader) DownloadFileFromURL(url string) (io.ReadCloser, error) {
-	resp, err := http.Get(url)
+func (fd *DefaultFileDownloader) DownloadFileFromURL(url string) ([]byte, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Minute)
+	defer cancel()
+
+	downloader := filedownloader.NewWithContext(ctx, retry.NewHTTPClient().StandardClient())
+	fileProvider := input.NewFileProvider(downloader)
+
+	contents, err := fileProvider.Contents(url)
+
 	if err != nil {
 		return nil, err
+	} else if contents == nil {
+		return nil, fmt.Errorf("failed to download file from %s", url)
 	}
 
-	if resp.StatusCode != http.StatusOK {
-		return resp.Body, fmt.Errorf("failed to download file from %s, status code: %d", url, resp.StatusCode)
-	}
-
-	return resp.Body, nil
+	return contents, nil
 }
 
 func NewDefaultFileDownloader(logger log.Logger) FileDownloader {
