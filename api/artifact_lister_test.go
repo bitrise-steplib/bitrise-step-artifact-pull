c9ee4681e7fc14b3e197c84386827758189306b4
Imre Kelényi <imre.kelenyi@bitrise.io>
2021-12-10 15:52:43 +0100

    Unified worker pools for concurrency (#41)

--- api/artifact_lister_test.go
+++ api/artifact_lister_test.go
@@ -2,7 +2,6 @@ package api
 
 import (
 	"errors"
-	"sync"
 	"testing"
 
 	"github.com/bitrise-io/go-utils/log"
@@ -38,64 +37,6 @@ func (m *MockBitriseAPIClient) ShowBuildArtifact(appSlug, buildSlug, artifactSlu
 	return r0, r1
 }
 
-func Test_showArtifact_returnsArtifact(t *testing.T) {
-	mockDownloadPath := "http://download.com"
-	mockArtifact := ArtifactResponseItemModel{Title: "artifact", DownloadPath: mockDownloadPath}
-	mockClient := &MockBitriseAPIClient{}
-	mockClient.
-		On("ShowBuildArtifact", mock.AnythingOfTypeArgument("string"), mock.AnythingOfTypeArgument("string"), mock.AnythingOfTypeArgument("string")).
-		Return(mockArtifact, nil)
-
-	lister := NewArtifactLister(mockClient, log.NewLogger())
-
-	showResultChan := make(chan showArtifactResult, 1)
-	wg := &sync.WaitGroup{}
-	wg.Add(1)
-	lister.showArtifact("app-slug", "build-slug", "artifact-slug", showResultChan, wg)
-	wg.Wait()
-
-	res := <-showResultChan
-
-	assert.NoError(t, res.err)
-	assert.Equal(t, mockArtifact, res.artifact)
-}
-
-func Test_listArtifactsOfBuild_returnsArtifactList(t *testing.T) {
-	mockArtifactList := []ArtifactListElementResponseModel{
-		{Slug: "artifact1"},
-		{Slug: "artifact2"},
-		{Slug: "artifact3"},
-	}
-
-	expectedArtifactList := []ArtifactResponseItemModel{
-		{Slug: "artifact1"},
-		{Slug: "artifact2"},
-		{Slug: "artifact3"},
-	}
-
-	mockClient := &MockBitriseAPIClient{}
-	mockClient.
-		On("ListBuildArtifacts", mock.AnythingOfTypeArgument("string"), mock.AnythingOfTypeArgument("string")).
-		Return(mockArtifactList, nil)
-	for _, a := range expectedArtifactList {
-		mockClient.On("ShowBuildArtifact", mock.AnythingOfTypeArgument("string"), mock.AnythingOfTypeArgument("string"), mock.AnythingOfTypeArgument("string")).
-			Return(a, nil).Once()
-	}
-
-	lister := NewArtifactLister(mockClient, log.NewLogger())
-
-	resultsChan := make(chan listArtifactsOfBuildResult, 1)
-	wg := &sync.WaitGroup{}
-	wg.Add(1)
-	lister.listArtifactsOfBuild("app-slug", "build-slug", resultsChan, wg)
-	wg.Wait()
-
-	res := <-resultsChan
-
-	assert.NoError(t, res.err)
-	assert.ElementsMatch(t, expectedArtifactList, res.artifacts)
-}
-
 type listConcurrencyTestCase struct {
 	maxConcurrentListCalls, maxConcurrentShowCalls int
 }
@@ -107,9 +48,11 @@ func Test_ListBuildArtifacts_concurrent_returnsArtifactListForMultipleBuilds(t *
 		{Slug: "artifact3"},
 		{Slug: "artifact4"},
 		{Slug: "artifact5"},
+		{Slug: "artifact6"},
+		{Slug: "artifact7"},
 	}
 
-	mockBuildSlugs := []string{"build-slug", "build-slug", "build-slug", "build-slug", "build-slug"}
+	mockBuildSlugs := []string{"build-slug", "build-slug", "build-slug", "build-slug", "build-slug", "build-slug", "build-slug"}
 
 	mockClient := &MockBitriseAPIClient{}
 	mockClient.
